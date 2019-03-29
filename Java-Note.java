d1f559e76291fe4c1787df651167f224cf347297
betrider <tlsfpem@gmail.com>
2019-03-29 09:14:31 +0900

    add

---
+++ Java-Note.java
@@ -0,0 +1,269 @@
+//#모두접기 Alt+0 / 모두펼치기 Alt+Shift+0 / 기호 양식순서 1. -> 1) -> (1)
+
+package D:\개발노트\코드 
+ 
+import https://devnudie.tistory.com/entry/eclipse%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9C%A0%EC%9A%A9%ED%95%9C-%EB%8B%A8%EC%B6%95%ED%82%A4 //STS단축키
+
+public class java {
+	
+	public static void main(String[] args){
+		
+		stsShortCutKey();
+		javaScript();
+		jsp();
+		java();
+		xml();
+		
+	}
+	
+	public void stsShortCutKey(){
+		
+		[블록]
+		Shift + ←, Shift + →	좌우로 한 캐릭터 단위 블록	
+		Shift + ↑, Shift + ↓	상하로 한 줄 단위 블록	세로 편집 모드에서 사용시 줄이 아닌 캐릭터로 블록
+		Ctrl + Shift + ←, Ctrl + Shift + →	좌우로 단어(캐릭터의 대소구별) 단위 블록	
+		Shift + Alt + ↑ ( ←, → )	연속된 문자열 블록	변수명이나 메소드명 블록(↑추가 입력시 중괄호 전체를 블록 가능하며 ←, → 키를 입력시 메소드 단위로 추가 블록 가능, ↓입력시 시간 역순으로 블록에서 제외)
+		Shift + Home, Shift + End	커서 위치 부터 줄의 처음/끝까지 블록	줄 전체를 블록하는 경우 커서를 줄의 처음이나 끝으로 이동(Home/End키) 시킨 후 사용
+		Ctrl + Shift + Home, Ctrl + Shift + End	커서 위치 부터 문서의 처음/끝까지 블록	
+
+		[검색]
+		Ctrl + F	기본 검색	찾기 메뉴를 여는 키로 ALT + D키를 추가로 입력해 위에서 아래로 하나씩 찾으면서 바꾸는데 유용
+		Ctrl + K, Ctrl + Shift + K	블럭한 문자열과 동일한 문자열 검색	변수의 사용 횟수 등을 추적
+		Ctrl + J	점증적 검색	키를 입력한 후 추가로 입력한 문자열과 동일한 내용으로 이동
+		Ctrl + Shift + L	프로젝트 전체 문자열 검색	STS의 기본 플러그인(이클립스에서는 설치해야 함)에 들어간 Quick Search 기능으로 전체 검색 기능인 Ctrl + H 대비 매우 빠른 속도
+		Ctrl + Shift + R	파일 이름 검색
+
+		[편집]
+		Ctrl + Space	컨텐트 어시스트	기본 자동완성 기능으로 두번 입력시 빠른 Method 생성 가능
+		Ctrl + 1	퀵 픽스	새로운 Method, Class 등 관련 객체등을 만들 거나 변경 시 빠르게 작업 할 수 있도록 도움
+		Ctrl + Shift + /, Ctrl + Shift + ＼	블록 주석	기본 주석인 Ctrl + /가 한정 된 언어에서 적용되는데 반해 언어 상관없이 동작
+		Ctrl + Shift + F	소스 코드 자동 정렬	동작 시 고정폭 문자열로 맞춰놓은 기본 형태가 무너지므로 규모가 작거나 스스로 개발한 파일 안에서만 사용하는 것을 권장
+		Ctrl + Shift + O	임포트 추가 및 정리	
+		Ctrl + D	한 줄 삭제	
+		Ctrl + Alt + ↑, Ctrl + Alt + ↓	한 줄 복제	
+		Alt + ↑, Alt + ↓	한 줄 이동	
+		Ctrl + Shift + Y, Ctrl + Shift + X	블럭한 문자열을 소문자/대문자로 변경	
+		Alt + Shift + J	설정 주석 자동 기입	
+		Alt + Shift + S	소스 메뉴 표시	VO, DTO 등의 Getter Setter 와 생성자 자동 완성에 유용, Getter Setter 빠르게 만들기 : R→ALT + A→Shift + Tab→Enter
+		Alt + Shift + T	리팩터 메뉴 표시	마지막 키인 T를 R(리네임), V(이동), C(메소드변경), M(메소드추출), L(지역변수화), I(인라인) 키로 대체하여 사용시 리팩터 메뉴에 바로 접근 가능
+		Alt + Shift + Z	서라운드 메뉴 표시	블록한 부분을 for, if, try/catch, do/while 등으로 감싸는 메뉴 호출
+		Alt + Shift + A	세로 편집 모드 설정/해제	설정 된 상태에서 기존 가로 블록이 세로를 지원
+
+		[이동]
+		Ctrl + Shift + ↑, Ctrl + Shift + ↓	클래스의 이전/다음 멤버로 이동	
+		Ctrl + Shift + P	매칭되는 괄호로 이동	
+		F3	요소의 선언된 곳으로 이동	
+		F4	현재 타입의 상속 구조의 표시	
+		Ctrl + Alt + H	호출부 표시	Ctrl + T와 함께 호출과 상속을 넘나들며 사용, 키 입력 후 화살표 엔터로 이동
+		Ctrl + T	상속부 표시	Ctrl + Alt + H와 함께 호출과 상속을 넘나들며 사용, , 키 입력 후 화살표 엔터로 이동
+		Ctrl + <, Ctrl + >	이전/다음 경고로 이동	에러 찾기
+		Ctrl + Q	마지막으로 편집한 위치로 이동	
+		Ctrl + L	줄 번호로 이동	예외 발생시 해당 줄번호로 이동
+		Alt + ←, Alt + →	파일을 열어본 순서에 따라 이전/다음 에디터로 이동	
+		Ctrl + E	열려있는 모든 에디터 표시	열려 있지만 화면에 표시 되지 않고 밀려서 숨겨져 있는 에디터들이 진한 글씨로 상단에 먼저 노출
+		Ctrl + PgUp, Ctrl + PgDn	다음/이전 열려있는 에디터로 이동	연 순서가 아닌 시각적인 순서에 따라 이동
+		Ctrl + F7, Ctrl + Shift + F7	다음/이전 뷰 이동	에디터에서 콘솔이나 서버로 이동하여 조작시 용이
+		Ctrl + F8, Ctrl + Shift + F8	다음/이전 퍼스펙티브 이동	스프링, 자바 EE, 디버그 모드 간 이동시 용이
+		F12	에디터로 이동	
+		Ctrl + M	최대/최소화	콘솔창을 크게 보거나 다른 요소를 배제할 때 용이
+
+		[실행]
+		Ctrl + F11	프로젝트 실행	전역 단축키
+		F11	디버그 모드 실행	전역 단축키
+		Ctrl + Shift + B	해당 줄의 브레이크 포인트 설정/해제	
+		F5	한 줄씩 디버그 실행하며 함수 일 경우 내부로 진입	
+		F6	한 줄씩 디버그 실행하며 내부 진입 하지 않음	
+		F7	디버그 실행 중 함수 밖으로 이동	
+		F8	다음 디버그 포인트까지 이동	
+		Ctrl + R	해당 줄 디버그 실행	
+		Ctrl + Alt + S  ( D, R )	실행 종료 (디버그, 실행)	지역 단축키 (서버 뷰에서만 동작)
+
+		참조:https://devnudie.tistory.com/entry/eclipse%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9C%A0%EC%9A%A9%ED%95%9C-%EB%8B%A8%EC%B6%95%ED%82%A4
+		
+	}
+	
+	public void javaScript(){
+		
+		Storage : 키-밸류 스토리지
+		localStorage -  인터넷 창 닫아도 데이터 사라지지않음 직접 삭제해줘야함
+		SessionStorage - 인터넷 창 닫으면 데이터 사라짐
+
+		<!DOCTYPE HTML>
+		<html>
+			<head> </head>
+			<body> </body>
+		</html>
+		
+	}
+	
+	public void jsp(){
+		
+		~디렉티브 형식 저장(JSP 페이지에 대한 설정 정보 지정)
+		page : JSP 페이지에 대한 정보 지정
+		taglib : JSP 페이지에서 사용할 태그 라이브러리 지정
+		include : JSP 페이지의 특정 영역에 다른 문서 포함
+
+		------------------------------------------------------------------------------
+		<%@page language="java" contentType="text/html; charset=UTF-8"%>
+		include header.jsp
+		include commonResource.jsp
+
+		<html> //header.jsp 에서 미리선언
+			<head> //header.jsp 에서 미리선언
+				<javascript>선언
+				
+				renderView() // 라벨,버튼,그리드선언,셋팅(Sencha Ext Js - 자바스크립트 프레임워크 사용) 
+				{
+				 grid01 = Ext.create('VC.grid.CommonGrid,{[셋팅값],columnDefine : [], buttonsConfig : [], listeners : [] }); //일반데이터는 columnDefine에만 선언되도 나오는데 popEditFieldset가 있을경우에는 popEditFieldset,columnDefine 양쪽에 선언되어야 보이는듯
+				 viewPort = Ext.create('VC.container.Viewport',{layout, items : [grid01], listeners}); // 화면에 버튼,그리드 등등 할당 - 반응형 웹(기기에 맞게 화면크기 셋팅 스마트폰,PC,아이패드 등등)
+				 settingPage();
+				}
+				
+
+				//변수선언
+				 var officeCd = "<%=userInfo.getDevision()%>";
+				 var officeNm = "<%=userInfo.getDevisionName()%>";
+
+				//settingPage
+				function settingPage(){
+				 if( CURRENT_MENUCODE == 'PTL823100' ){
+				  Ext.getCmp('newBtnBorder').destroy();
+				  Ext.getCmp('newBtnDomestic').destroy();
+				 }
+				}
+				
+				//Function
+				function search(){
+				 //Validation
+				 grid01.search(viewPort, 경로, params, false, function(result, successYn) //경로는 java함수까지 - '/fis/portalTrkExHawbRetServiceHTSV/search'
+				}
+
+				function save(){
+				 //Validation
+				 Ext.MessageBox.show({title,msg,buttons,icon,
+							 fn : function(btn)
+							{
+							 if(btn === 'yes')
+							 {
+							  Ext.Ajax.request({url,jsonData,params,
+									call back : function(options, success, response)
+									{
+									 var json = Ext.decode(response.responseText); //반환값 넘어옴 
+									 if(json["ErrorMsg"] === "OK")
+									 {
+									  Ext.MessageBox.show()
+									 }
+									 else
+									 {
+									  Ext.MessageBox.show()
+									 }
+									}
+										 });
+							 }	
+							 else if (btn === 'no')
+							 {
+							  return;
+							 }			
+							}
+							});
+				}
+
+				</script>
+			</head>
+			<body>
+			</body>
+		</html>
+
+		------------------------------------------------------------------------------
+
+		*head,body 차이
+		head : js 처리가 완료되어야 화면이 열린다.(가변운화면에서 좋음)
+		body : 화면이 열린 후 js가 처리된다.(무거운화면에서 좋음)
+		
+	}
+	
+	public void java(){
+		
+		package ~
+		import ~
+		public class ~ (extends ~)
+			public~
+
+			public~
+
+		*xml(쿼리) 
+		파라메터 입력
+		paramMap.put("OFFICE_CD", officeCd);
+		paramMap.put("DYNAMIC_QUERY", DYNAMIC_QUERY);
+		
+		*어노테이션 - 메타데이터
+		@Service
+		@Autowired
+		@SuppressWarnings
+		@Repository
+		@Authorize
+
+		*MYBATIS - 객체지향 어플리케이션에서 관계형 데이터베이스를 쉽게 사용할 수 있도록 도와주는 데이터 맵핑 프레임워크이다.
+		*JSON(JavaScript Object Notation) name-value 형식의 쌍
+		{
+		  "firstName": "Kwon",
+		  "lastName": "YoungJae",
+		  "email": "kyoje11@gmail.com"
+		}
+		list 형식
+		{
+		  ["firstName": "Kwon","lastName": "YoungJae","email": "kyoje11@gmail.com"]
+		  ["firstName": "Kwon","lastName": "YoungJae","email": "kyoje11@gmail.com"]
+		  ["firstName": "Kwon","lastName": "YoungJae","email": "kyoje11@gmail.com"]
+		}
+		*MAVEN - 라이브러리 라이프 사이클 관리도구(pom.xml)
+		업데이트 방법 프로젝트 우클릭 -> maven -> Update Project(Alt+F5)
+
+		3.자료구조
+		Collection(key-value, 중복x)
+		Map - map interface
+		HashMap(key-value쌍,중복x,null o)
+		TreeMap(key-value쌍,중복x,정렬 o)
+		HashTable(key-value쌍,중복x,null x)
+
+		4.var,let,const
+		var - function scoped
+		let - block scoped
+		const - 초기선언후 재할당 불가능
+
+		5.extend,implement
+		extend - 상속(메소드 그대로 사용가능)
+		implement - 인터페이스(메소드 다시 선언후 사용가능)
+		https://hashcode.co.kr/questions/471/implements%EC%99%80-extends%EC%9D%98-%EC%B0%A8%EC%9D%B4%EC%A0%90%EC%9D%B4-%EB%AD%90%EA%B3%A0-%EA%B0%81%EA%B0%81-%EC%96%B8%EC%A0%9C-%EC%93%B0%EB%8A%94%EA%B1%B4%EA%B0%80%EC%9A%94
+
+		6.콘솔 쿼리로그 해결방법(logback.xml)
+		logback.xml 에서 sql,resultset TRACE로 권한변경
+		
+	}
+	
+	public void xml(){
+		
+		쿼리
+		<select id="SELECT_PORTAL_FIS_TRACKING_HTSV" resultType="java.util.HashMap">
+		</select>
+		블럭
+		<if test='PAGE_SIZE != null and !PAGE_SIZE.equals("")'>
+		</if>
+		파라메터
+		#{OFFICE_CD} - 문자열
+		${DYNAMIC_QUERY} - 쿼리
+		CDATA 쿼리에 닫힘태그 있을경우 사용함
+		<![CDATA[
+		]]>
+		!!xml id 충돌나면 컴파일 에러남, resultMap id도 중복x 필수확인
+		
+	}
+
+}
+
+
+
+
+
+			
+		
---
+++ Sql-Note.sql
@@ -0,0 +1,316 @@
+﻿-- 모두접기 Alt+0 / 모두펼치기 Alt+Shift+0 / 기호 양식순서 1. -> 1) -> (1)
+
+CREATE OR REPLACE PROCEDURE ORACLE11G
+(
+      CUSTOM_NUMBER     IN  VARCHAR2
+    , R_ERROR_CODE      OUT INTEGER
+    , R_ERROR_MSG       OUT VARCHAR2
+) AS
+
+V_RTNCD         		VARCHAR2(20 CHAR)  := '';
+V_RTNMSG        		VARCHAR2(500 CHAR) := '';
+USER_CREATE_EXCEPTION   EXCEPTION;   
+
+BEGIN
+	
+	BEGIN --1.command
+	
+		1.SELECT * FROM 테이블명 GROUP BY 컬럼명 ORDER BY 컬럼명
+		2.INSERT INTO 테이블명 (컬럼명) VALUES (데이터)
+		3.UPDATE 테이블명 SET 컬럼명 = 데이터 WHERE 컬럼 = 데이터
+		4.DELETE 테이블명 WHERE 컬럼 = 데이터
+		5.INSERT INTO 테이블명 SELECT * FROM 테이블명 -- INSERT-SELECT 형식 
+		6.CREATE TABLE 테이블명 AS SELECT * FROM 테이블명 -- CREATE-SELECT 형식
+		7.MERGE 형식 
+		
+		   MERGE INTO 테이블명
+				USING (가상테이블 생성)
+				   ON(테이블명.컬럼명 = 가상테이블명.컬럼명) --조건
+		   WHEN MATCHED THEN
+				   UPDATE SET 컬럼명 = 데이터
+		   WHEN NOT MATCHED THEN
+				   INSERT (컬럼명) VALUES (데이터)
+		   ))) --보정
+		8.WITH 형식 (
+			WITH ALIAS명 AS ( SUB쿼리 ) , ALIAS명 AS ( SUB쿼리 )
+			SELECT 컬럼명 FROM ALIAS명
+		)
+		9.JOIN 형식 (
+			(1)암시적 조인 - SELECT * 
+							   FROM FIRST_TABLE A1 
+						 INNER JOIN SECOND_TABLE A2 --INNER JOIN
+								 ON A1.NO1 = A2.NO1
+						  LEFT JOIN SECOND_TABLE A2 --LEFT JOIN
+								 ON A1.NO1 = A2.
+						 RIGHT JOIN SECOND_TABLE A2 --RIGHT JOIN
+								 ON A1.NO1 = A2.NO1
+			(2)묵시적 조인 - SELECT * 
+			                   FROM FIRST_TABLE A1, SECOND_TABLE A2 
+							   WHERE A1.NO1    = A2.NO1    --INNER JOIN
+							   WHERE A1.NO1(+) = A2.NO1    --LEFT OUTER JOIN
+							   WHERE A1.NO1    = A2.NO1(+) --RIGHT OUTER JOIN
+		)
+		10.CASE WHEN 컬럼명 = 데이터 THEN 데이터 ELSE 데이터 END
+		11.DECODE(원본데이터,비교데이터,TRUE값,FALSE값) --원본데이터 와 비교데이터 동일하면 TRUE값 반환 아닐경우 FALSE값 반환
+		12.문자변환함수 ( 
+			(1)REPLACE(원본데이터,비교데이터,변환데이터) --문자 묶음단위로 변환
+				SELECT REPLACE('123456','345','666') FROM DUAL => '1236666'
+			(2)TRANSLATE(원본데이터,비교데이터,변환데이터) --문자 한개단위로 변환
+				SELECT REPLACE('123456','246','@$^') FROM DUAL => '1@3$5^'
+		)
+		13.NULL값 변환 (
+			(1)NVL(데이터1,데이터2)           -- 데이터1이 NULL이면 데이터2 반환
+			(2)NVL2(데이터1,데이터2,데이터3)  -- 데이터1이 NULL이 아니면 데이터2 반환 NULL이면 데이터3 반환
+			(3)NULLIF(데이터1,데이터2)      -- 데이터1와 데이터2가 같으면 NULL반환 아닐경우에는 데이터1 반환
+			(4)COALESCE(데이터,데이터.....) --NULL이 아닌 첫번째 값 반환
+				SELECT COALESCE(NULL, NULL, 'third_value', 'fourth_value') => 'third_value'
+		)
+		14.문자열 자르기 (
+			(1)SUBSTR --SUBSTR(데이터,시작위치,길이) CHAR기준
+				SELECT SUBSTR('가나다라마',1,3) FROM DUAL => '가나다' 
+			(2)SUBSTRB --SUBSTRB(데이터,시작위치,길이) BYTE기준
+				SELECT SUBSTR('가나다라마',1,3) FROM DUAL => '가'    
+		)
+		15.문자열 길이체크 (
+			(1)LENGTH : SELECT LENGTH('가나다') FROM DUAL => 3   --LENGTH(데이터) CHAR기준
+			(2)LENGTHB : SELECT LENGTHB('가나다') FROM DUAL => 9 --LENGTHB(데이터) BYTE기준
+		)
+		16.변환함수 (
+			(1)TO_CHAR -- 숫자나 날짜를 문자열로 변환
+				TO_CHAR(SYSDATE,'yyyy-mm-dd am hh24:mi:ss') => '2019-03-28 오후 15:38:05'
+			(2)TO_NUMBER -- 문자를 숫자로 변환 
+				SELECT TO_NUMBER('0123456789') FROM DUAL => 123456789
+			(3)TO_DATE -- 문자를 날짜로 변환 
+				TO_DATE('2019-03-28 14:47:01','YYYY-MM-DD hh24:MI:SS') => '2019-03-28 오후 2:47:01'
+		)
+		17.문자열 합치기 (
+			(1)WM_CONCAT(컬럼명) --ORACLE 10G
+			(2)LISTAGG(컬럼명,',') WITHIN GROUP(ORDER BY 컬럼명) -- ORACLE 11G
+		)
+		18.조건제어 (
+			필드명 = 데이터
+			필드명 LIKE (%데이터%)
+			필드명 IN (데이터1,데이터2)
+			필드명 IS NULL 
+			필드명 IS NOT NULL
+			EXISTS (SUB쿼리)
+		)
+		19.부등호 : = 같다, <> or != 같지않다 
+		20.한줄 띄우기 CHR(13)||CHR(10)
+		21.대소문자 변환 UPPER , 소문자로 변환 LOWER , 첫글자 대문자 INITCAP
+		22.공백 없애기 TRIM(양쪽),LTRIM(왼쪽),Rtrim(오른쪽)
+		23.문자 채우기 LPAD('oracle',10,'x') => 'xxxxoracle' , RPAD('oracle',10,'x') => 'oraclexxxx'
+		24.문자 거꾸로 출력 REVERSE(데이터)
+		25.그룹함수 MAX(최대값),MIN(최소값),AVG(평균값),SUM(합계),COUNT(개수)
+		26.집합연산자 UNION(중복제거O),UNION ALL(중복제거X),INTERSECT(교집합),MINUS(차집합) --INTERSECT 양쪽에 A,B데이터가 있는경우 , MINUS B에는 없고 A에는 있는 데이터
+		27.순위결정함수 (
+			(1)RANK       : RANK() OVER(ORDER BY 컬럼명 DESC) AS RANK                   -- (50,40,40,30,20) => 1,2,2,4,5
+			(2)DENSE_RANK : ROW_NUMBER() OVER(ORDER BY 컬럼명 DESC) AS RANK             -- (50,40,40,30,20) => 1,2,2,3,4
+			(3)ROW_NUMBER : RANK() OVER (PARTITION BY 컬럼명 ORDER BY 컬럼명 DESC) RANK -- (50,40,40,30,20) => 1,2,3,4,5
+		)
+		28.OVER함수 ( -- ORDER BY,GORUP BY 서브쿼리를 개선하기 위해 나온 함수. 서브쿼리라 그룹안에서 쓰려면 해당컬럼도 그룹바이에 추가해줘야 한다.
+			COUNT(*)OVER() : 전체행 카운트
+			COUNT(*)OVER(PARTITION BY 컬럼) : 그룹단위로 나누어 카운트
+
+			MAX(컬럼)OVER() : 전체행 중에 최고값
+			MAX(컬럼)OVER(PARTITION BY 컬럼) : 그룹내 최고값
+
+			MIN(컬럼)OVER() : 전체행 중에 최소값
+			MIN(컬럼)OVER(PARTITION BY 컬럼) : 그룹내 최소값
+
+			SUM(컬럼)OVER() : 전체행 합
+			SUM(컬럼)OVER(PARTITION BY 컬럼) : 그룹내 합
+
+			AVG(컬럼)OVER() : 전체행 평균
+			AVG(컬럼)OVER(PARTITION BY 컬럼) : 그룹내 평균
+
+			STDDEV(컬럼)OVER() : 전체행 표준편차
+			STDDEV(컬럼)OVER(PARTITION BY 컬럼) : 그룹내 표준편차
+		)
+		29.그룹함수 (
+			참조:https://betrider.tistory.com/58
+			(1)ROLLUP : SELECT COL1,COL2,SUM(COL3) FROM TABLE_NAME GROUP BY ROLLUP(COL1,COL2) --일반집계
+			(2)CUBE   : SELECT COL1,COL2,SUM(COL3) FROM TABLE_NAME GROUP BY CUBE(COL1,COL2)   --다차원집계
+			*GROUPING : (CASE WHEN GROUPING(COL1) = '1' THEN '합계' ELSE COL1 END)            --총계를 나타내는 결과물이면 1, 아니면 0을 반환한다.
+ 		)
+		30.계층형쿼리 (
+				SELECT LEVEL AS LEV
+					 , A1.*
+			 	 FROM 테이블명 A1
+				WHERE 컬럼명 = 데이터                 --전체데이터 조건
+		   START WITH 컬럼명 = 데이터                 --부모 노드 데이터 조건
+		   CONNECT BY PRIOR 자식 컬럼명 = 부모 컬럼명 --데이터 연결
+		  ORDER SIBLINGS BY 컬럼명 ASC                --정렬
+		  
+			  SELECT ROWNUM 
+				FROM DUAL 
+		  CONNECT BY LEVEL < 3 => ROWNUM
+									1
+									2
+									3
+		)
+		31.권한부여 (
+			해당 계정으로 접속 또는 SYSTEM 계정으로 접속 후
+			GRANT SELECT, INSERT, DELETE, UPDATE ON TABLE_NAME TO USER_NAME
+		)
+		32.소수점 포맷 SELECT TO_CHAR('12345.12345','FM999990.000') FROM DUAL => 12345.123
+	
+	END
+	
+	BEGIN --2.toadShortCutKey
+		
+		F2 하단패널 ON/OFF
+		SHIFT + F2 하단패털 꽉채우기
+		F3 다음검색
+		F4 & Ctrl+Click DESC
+		F5,F9 실행
+		F6 커서 에디터&패널 전환
+		F7 화면클리어
+		F8 쿼리 히스토리
+
+		CTRL + F9 - 오류체크
+		SHIFT + F9 - 커서위치 쿼리문 실행
+
+		CTRL + U,L - 대문자,소문자전환
+		CTRL + F - 검색
+		CTRL + R - 문자변경
+		CTRL + O - 파일열기
+		CTRL + T , CTRL + . - 컬럼 OR 테이블리스트
+		CTRL + B 주석
+		CTRL + SHIFT + B 주석해제
+		
+		*CTRL + M - 쿼리 -> SQL문으로 변환
+
+		SQL = " SELECT  "
+		SQL = SQL & "OFFICE_CD, MNGT_NO, FARE_SEQ,  "
+		SQL = SQL & "   DEL_HM "
+		SQL = SQL & "FROM ELVIS_TEST.ACT_INV_DTL_DEL_LOG "
+		
+		CTRL + P - SQL문 ->쿼리로 변환
+		CTRL + E Explain Plan(튜닝할때 사용)
+		CTRL + Insert - Data Grid에서 값 복사할때 컬럼명도 복사
+		CTRL+SHIFT+F 쿼리정렬 (원하는 범위 선택후 사용하면됨)
+
+		*Data Grid에서 자료수정 (예제 테이블:FMS_HBL_MST)
+
+		F11 디버깅
+
+		SHIFT + F8 디버깅 한단계씩 진행
+
+		View - Code Snippets 오라클 함수 목록
+
+		*간단한 쿼리문 가지고 오기
+
+		Schema Browser - 해당테이블 우클릭 - Generate Statement - Insert,Select,Update 중 클릭 후 붙여넣기
+		
+	END
+	
+	BEGIN --3.query
+					
+		=================== **select 문의 처리순서
+		select 컬럼명 --(5)
+		from 테이블명 --(1)
+		where 조건절  --(2)
+		//(1),(2)를 한 것은 해당 테이블에서 조건에 만족하는 행을 메모리(RAM)에 로드시킨것
+		group by절 --(3)
+		having 그룹함수 조건절 --(4)
+		order by절 --(6)
+		===================
+		
+		!!!데이터 그리드에서 바로 수정하기
+			EDIT TABLE_NAME WHERE 컬럼명 = 데이터                      --전체컬럼
+			or
+			SELECT ROWID,A.* FROM TABLE_NAME A WHERE A.컬럼명 = 데이터 --일부컬럼 OR 전체컬럼
+			
+		!!!테이블명 검색 또는 컬럼명 검색 또는 인덱스 검색
+			SELECT * FROM ALL_TAB_COLUMNS WHERE COLUMN_NAME = 데이터
+			OR
+			SELECT * FROM ALL_TAB_COLUMNS WHERE TABLE_NAME  = 데이터
+			OR
+			SELECT * FROM ALL_INDEXES WHERE INDEX_NAME  = 데이터
+
+		!!!프로시저 내용 검색
+			SELECT * FROM USER_SOURCE WHERE TYPE = 'PROCEDURE' AND TEXT LIKE '%데이터%'
+
+		!!!함수 내용 검색
+			SELECT * FROM USER_SOURCE WHERE TYPE = 'FUNCTION' AND TEXT LIKE '%데이터%'
+			
+		*IP확인
+			select SYS_CONTEXT('USERENV', 'IP_ADDRESS', 15) ipaddr from dual
+		
+		*최신값 하나씩만 가지고오기
+			SELECT * FROM (SELECT A.*,ROW_NUMBER()OVER(PARTITION BY COL1 ORDER BY COL2 DESC) RN FROM TABLE_NAME A) WHERE RN=1
+			
+		*파라메터 없으면 패스 있으면 조건 태우는 방법
+			SELECT * FROM TABLE_NAME WHERE (PARAM1 IS NULL OR 컬럼명 = PARAM1)
+		
+		*합계와 총합계 같이표시
+			SELECT DO_NO,DO_ITEM,SUM(DO_QTY),SUM(DO_QTY) OVER(PARTITION BY DO_NO) FROM FMS_DO_DTL GROUP BY DO_NO,DO_ITEM,DO_QTY
+		
+		*드랍 테이블 복구
+			FLASHBACK TABLE FMS_HBL_OTH TO BEFORE DROP
+			
+		*IN 1000개 넘어갈때
+			SELECT * FROM XXX WHERE ID IN (1,2,3,..) -> SELECT * FROM XXX WHERE (0,ID) IN ((0,1), (0,2), (0,3)..)
+			
+		*다른 데이터베이스 컬럼 비교하기
+			SELECT COLUMN_NAME,WM_CONCAT(TABLE_NAME),COUNT(TABLE_NAME),WM_CONCAT(COPY) FROM
+			(
+			  SELECT COLUMN_NAME,'A_FMS_HBL_MST' AS TABLE_NAME, 'FMS_HBL_MST' AS COPY
+			  FROM ALL_TAB_COLUMNS
+			  WHERE TABLE_NAME='FMS_HBL_MST' AND OWNER = 'ELVIS_MCI' --MSSQL링크 대소문자 구분해야됨
+			  UNION ALL
+			  SELECT COLUMN_NAME,'B_FMS_HBL_OTH' AS TABLE_NAME , 'FMS_HBL_OTH' AS COPY
+			  FROM ALL_TAB_COLUMNS
+			  WHERE TABLE_NAME='FMS_HBL_OTH' AND OWNER = 'ELVIS_MCI'
+			  UNION ALL
+			  SELECT COLUMN_NAME,'C_FMS_HBL_ED' AS TABLE_NAME , 'FMS_HBL_ED' AS COPY
+			  FROM ALL_TAB_COLUMNS
+			  WHERE TABLE_NAME='FMS_HBL_ED' AND OWNER = 'ELVIS_MCI'
+			  UNION ALL
+			  SELECT COLUMN_NAME,'D_FMS_HBL_DOC' AS TABLE_NAME , 'FMS_HBL_DOC' AS COPY
+			  FROM ALL_TAB_COLUMNS
+			  WHERE TABLE_NAME='FMS_HBL_DOC' AND OWNER = 'ELVIS_MCI'
+			  )
+			  GROUP BY COLUMN_NAME
+			  ORDER BY WM_CONCAT(TABLE_NAME) ASC 
+			  
+		*계층형 쿼리 연결된값 전부 가져오기
+			SELECT *
+			  FROM ADT_CSY_MENU T1
+			 WHERE APPCODE = 'C-PORTAL' AND MENUCODE LIKE 'PTL66%'
+			 START WITH SUPERMENUCODE = '-1'
+			CONNECT BY PRIOR MENUCODE = SUPERMENUCODE
+			 ORDER SIBLINGS BY ORDERS ASC
+			 
+		*합계와 총합계 같이표시
+		SELECT DO_NO,DO_ITEM,SUM(DO_QTY),SUM(DO_QTY) OVER(PARTITION BY DO_NO) FROM FMS_DO_DTL GROUP BY DO_NO,DO_ITEM,DO_QTY
+		
+	END
+	
+	
+EXCEPTION
+	WHEN NO_DATA_FOUND THEN
+           R_ERROR_CODE := -1;
+           
+	BEGIN --4.error
+	
+		*ORA-12899 
+			DB링크로 INSERT할때 열에대한값이너무큼 오류나오면
+			BACK 테이블로 CRATE TABLE SELECT 로 데이터 받은다음에 데이터만 다시 옮겨주자
+		*ORA-01045
+			GRANT CREATE SESSION TO 유저명
+		
+	END
+	
+		   R_ERROR_MSG := 'DATA NOT FOUND';
+           RETURN;
+	WHEN USER_CREATE_EXCEPTION THEN
+           R_ERROR_CODE := -1;
+           R_ERROR_MSG := V_RTNMSG;
+           RETURN;
+	WHEN OTHERS THEN
+           R_ERROR_CODE := -1;
+           R_ERROR_MSG := SQLERRM;
+           RETURN;
+END
